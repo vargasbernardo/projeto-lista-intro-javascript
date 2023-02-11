5b4f2eabfe0f595ed47f6c75fd20b5fc31a8562e
vargasbernardo <vargasbernardo97@gmail.com>
2023-02-11 10:24:55 -0300

    Alguns exercicios feitos

--- exercicios.js
+++ exercicios.js
@@ -16,58 +16,109 @@ function imprimeMensagem() {
 
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
-// EXERCÍCIO 01
+// EXERCÍCIO 01 (FEITO)
 function calculaAreaRetangulo() {
   // implemente sua lógica aqui
+  const alturaUser = prompt('Digite a altura: ')
+  const larguraUser = prompt('Digite a largura: ')
+  const area = alturaUser * larguraUser
+  console.log(area)
 
 }
 
-// EXERCÍCIO 02
+// EXERCÍCIO 02 (FEITO)
 function imprimeIdade() {
   // implemente sua lógica aqui
+  const anoAtual = prompt('Digite o ano atual:')
+  const anoDeNascimento = prompt('Digite o ano de nascimento: ')
+  const idade = anoAtual - anoDeNascimento
+
+  console.log(idade)
 
 }
 
-// EXERCÍCIO 03
+// EXERCÍCIO 03 (FEITO)
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
+  const imc = peso / (altura * altura)
+
+  return imc
 
 }
 
-// EXERCÍCIO 04
+// EXERCÍCIO 04 (FEITO)
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+  const nomeUsuario = prompt('Digite seu nome: ')
+  const idadeUsuario = Number(prompt('Digite sua idade: '))
+  const emailUsuario = prompt('Digite seu email: ')
+  
+  console.log(`Meu nome é ${nomeUsuario}, tenho ${idadeUsuario} anos, e o meu email é ${emailUsuario}.`)
 
 }
 
-// EXERCÍCIO 05
+// EXERCÍCIO 05 (FEITO)
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
+  const corFavorita1 = prompt('Qual a cor que voce mais gosta?')
+  const corFavorita2 = prompt('Qual a segunda cor que voce mais gosta?')
+  const corFavorita3 = prompt('Qual a terceira cor que voce mais gosta?')
+  let newArray = []
+  // newArray[0] = corFavorita1
+  // newArray[1] = corFavorita2
+  // newArray[2] = corFavorita3
+  newArray.push(corFavorita1)
+  newArray.push(corFavorita2)
+  newArray.push(corFavorita3)
+
+  console.log(newArray)
 
 }
 
-// EXERCÍCIO 06
+// EXERCÍCIO 06 (FEITO)
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
+  const recebeString = string
+  const stringMaiuscula = recebeString.toUpperCase()
+  return stringMaiuscula
 
 }
 
-// EXERCÍCIO 07
+// EXERCÍCIO 07 (FEITO)
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
+  const calculoPrejuizo = custo / valorIngresso
+
+  return calculoPrejuizo
 
 }
 
-// EXERCÍCIO 08
+// EXERCÍCIO 08 (FEITO)
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
+  const tamanhoString1 = string1.length
+  const tamanhoString2 = string2.length
+
+  // const comparaStrings = tamanhoString1 === tamanhoString2 === true
+  // return comparaStrings
+
+  if (tamanhoString1 === tamanhoString2) {
+    return true
+  } else {
+    return false
+  }
+
 
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
+   let newArray = [array]
+
+   return newArray.first()
+
 
 }
 
@@ -86,12 +137,22 @@ function trocaPrimeiroEUltimo(array) {
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
+  const compararStrings = string1.toLowerCase() === string2.toLowerCase() === true
+  return compararStrings
 
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
+  const anoAtual = Number(prompt('Informe o ano atual: '))
+  const anoDeNascimentoPessoa = Number(prompt('Informe o ano de nascimento: '))
+  const anoEmissaoRG = Number(prompt('Informe o ano em que o RG foi emitido: '))
+
+  if (anoAtual - anoDeNascimentoPessoa <= 20) {
+    
+
+  }
 
 }
 
